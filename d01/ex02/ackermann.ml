d7b81f31479d5012d5975ffe93ac03b4a0807dac
Estelle TIEBERGHIEN <etieberg@e1r4p10.42.fr>
2019-10-14 15:37:35 +0200

    ex02 fixed

--- d01/ex02/ackermann.ml
+++ d01/ex02/ackermann.ml
@@ -1,23 +1,20 @@
 let rec ackermann m n =
-    if m = 0 then
-       n + 1
-    else if m > 0 then
-       m
+    if m < 0 then
+        m
+    else if m = 0 then
+        n + 1
+    else if n = 0 then
+        ackermann (m - 1) 1
     else
-       if n = 0 then
-          ackermann (m - 1) 1
-       else
         ackermann (m - 1) (ackermann m (n - 1))
 
 let main () =
-   print_int (ackermann (-1) 7);
-   print_char '\n';
-(*   print_int (ackermann 0 0);
+   print_int (ackermann 0 0);
    print_char '\n';
    print_int (ackermann 2 3);
    print_char '\n';
    print_int (ackermann 4 1);
-   print_char '\n';*)
+   print_char '\n';
    print_int (ackermann (-1) 7);
    print_char '\n'
 
