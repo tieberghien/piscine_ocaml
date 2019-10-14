9f7ed421454c6263b9a802fb2a81c90f7574b492
Estelle TIEBERGHIEN <etieberg@e1r4p10.42.fr>
2019-10-14 16:38:14 +0200

    ex04 done

--- d01/ex04/fibonacci.ml
+++ d01/ex04/fibonacci.ml
@@ -1,20 +1,24 @@
-let fibonacci n = 
-   let rec loop i a b =
-      if i = n then 
-         a
-      else 
-         loop (i + 1) b (a + b)
-   in 
-   loop 0 0 1
+let fibonacci n =
+    let rec loop n a b =
+        if n < 0 then
+            (-1)
+        else if n == 0 then
+            a
+        else if n == 1 then
+            b
+		else 
+            loop (n - 1)  (b)  (a + b)
+    in
+    loop n 0 1
 
 let main () =
    print_int (fibonacci (-42));
    print_char '\n';
    print_int (fibonacci 1);
-   print_char '\n'
-(*   print_int (fibonacci 3);
+   print_char '\n';
+   print_int (fibonacci 3);
    print_char '\n';
    print_int (fibonacci 6);
-   print_char '\n' *)
+   print_char '\n'
 
 let () = main ()
